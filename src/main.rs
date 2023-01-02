c81848a4deb499f657b33332c90d8203c102b9c2
k1nho <kinhong99@gmail.com>
2023-01-02 18:18:07 -0500

    config call in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use rust_tab_opener::{run, Mode};
+use rust_tab_opener::{config, run};
 use std::env;
 
 fn main() {
     let args = env::args();
-    run(Mode::Exec);
+    let config = config(args).unwrap();
+    println!("config is {:?}", config);
+    // run(config)
 }
